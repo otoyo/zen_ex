0914a035a171b19333783c25972b9b632586de2c
otoyo <hiroki.t.1988@gmail.com>
2017-04-24 14:24:11 +0900

    Initial commit

---
+++ lib/zendex.ex
@@ -0,0 +1,18 @@
+defmodule Zendex do
+  @moduledoc """
+  Documentation for Zendex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zendex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
