396768e610bca5a3029891dad095749af6dcfc55
otoyo <hiroki.t.1988@gmail.com>
2017-04-28 14:45:03 +0900

    Add ticket_form_id to Ticket

--- lib/zendex/entities/ticket.ex
+++ lib/zendex/entities/ticket.ex
@@ -3,6 +3,6 @@ defmodule Zendex.Entity.Ticket do
   defstruct [:id, :url, :external_id, :created_at, :updated_at, :type, :subject, :raw_subject,
              :description, :priority, :status, :recipient, :requester_id, :submitter_id, :assignee_id,
              :organization_id, :group_id, :collaborator_ids, :forum_topic_id, :problem_id, :has_incidents,
-             :due_at, :tags, :via, :custom_fields, :satisfaction_rating, :sharing_agreement_ids]
+             :due_at, :tags, :via, :ticket_form_id, :custom_fields, :satisfaction_rating, :sharing_agreement_ids]
 
 end
