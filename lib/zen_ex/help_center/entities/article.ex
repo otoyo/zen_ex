a62737295d3c8fd3f85b9c27fbf378371da91564
otoyo <hiroki.t.1988@gmail.com>
2017-05-28 20:23:56 +0900

    Add entities for HelpCenter

---
+++ lib/zen_ex/help_center/entities/article.ex
@@ -0,0 +1,10 @@
+defmodule ZenEx.HelpCenter.Entity.Article do
+
+  defstruct [:id, :url, :html_url, :title, :body, :locale, :source_locale, :author_id,
+             :comments_disabled, :outdated_locales, :outdated, :label_names, :draft,
+             :promoted, :position, :vote_sum, :vote_count, :section_id, :created_at, :updated_at]
+
+  @moduledoc """
+  Article entity corresponding to Zendesk HelpCenter Article
+  """
+end
