d45cc3ef8d739ee58ec7975d9474e9e0fa9b8099
otoyo <hiroki.t.1988@gmail.com>
2017-06-17 19:59:53 +0900

    Use new HTTPClient at HelpCenter.Model.Section

--- spec/zen_ex/help_center/models/section_spec.exs
+++ spec/zen_ex/help_center/models/section_spec.exs
@@ -1,7 +1,6 @@
 defmodule ZenEx.HelpCenter.Model.SectionSpec do
   use ESpec
 
-  alias ZenEx.HTTPClient
   alias ZenEx.HelpCenter.Entity.{Category, Section}
   alias ZenEx.HelpCenter.Model
 
@@ -10,7 +9,7 @@ defmodule ZenEx.HelpCenter.Model.SectionSpec do
   end
 
   let :json_sections do
-    ~s({"sections":[{"id":35436,"name":"Help I need somebody!","locale":"en-us","category_id":112233},{"id":20057623,"name":"Not just anybody!","locale":"en-us","category_id":112233}]})
+    ~s({"count":2,"sections":[{"id":35436,"name":"Help I need somebody!","locale":"en-us","category_id":112233},{"id":20057623,"name":"Not just anybody!","locale":"en-us","category_id":112233}]})
   end
   let :sections do
     [struct(Section, %{id: 35436, name: "Help I need somebody!", locale: "en-us", category_id: category().id}),
@@ -26,44 +25,35 @@ defmodule ZenEx.HelpCenter.Model.SectionSpec do
   let :response_404, do: %HTTPotion.Response{status_code: 404}
 
   describe "list" do
-    before do: allow HTTPClient |> to(accept :get, fn(_) -> response_sections() end)
-    it do: expect Model.Section.list("en-us") |> to(eq sections())
-    it do: expect Model.Section.list("en-us", category().id) |> to(eq sections())
+    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_sections() end)
+    it do: expect Model.Section.list("en-us") |> to(be_struct ZenEx.Collection)
+    it do: expect Model.Section.list("en-us").entities |> to(eq sections())
+    it do: expect Model.Section.list("en-us", category().id).entities |> to(eq sections())
   end
 
   describe "show" do
-    before do: allow HTTPClient |> to(accept :get, fn(_) -> response_section() end)
+    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_section() end)
     it do: expect Model.Section.show("en-us", section().id) |> to(eq section())
   end
 
   describe "create" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_section() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_section() end)
     it do: expect Model.Section.create(section()) |> to(be_struct Section)
   end
 
   describe "update" do
-    before do: allow HTTPClient |> to(accept :put, fn(_, _) -> response_section() end)
+    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_section() end)
     it do: expect Model.Section.update(section()) |> to(be_struct Section)
   end
 
   describe "destroy" do
     context "response status_code: 204" do
-      before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_204() end)
+      before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_204() end)
       it do: expect Model.Section.destroy(section().id) |> to(eq :ok)
     end
     context "response status_code: 404" do
-      before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_404() end)
+      before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_404() end)
       it do: expect Model.Section.destroy(section().id) |> to(eq :error)
     end
-  end
-
-  describe "_create_sections" do
-    subject do: Model.Section._create_sections response_sections()
-    it do: is_expected() |> to(eq sections())
-  end
-
-  describe "_create_section" do
-    subject do: Model.Section._create_section response_section()
-    it do: is_expected() |> to(eq section())
   end
 end
