2a9e9df1dfcf453fe1193214bfc971910cd6fe8d
otoyo <hiroki.t.1988@gmail.com>
2017-06-17 14:42:38 +0900

    Use new HTTPClient at Model.User

--- lib/zen_ex/core/models/user.ex
+++ lib/zen_ex/core/models/user.ex
@@ -13,12 +13,12 @@ defmodule ZenEx.Model.User do
   ## Examples
 
       iex> ZenEx.Model.User.list
-      [%ZenEx.Entity.User{id: xxx, name: "xxx", ...}, ...]
+      %ZenEx.Collection{}
 
   """
-  @spec list :: list(%User{})
+  @spec list :: %ZenEx.Collection{}
   def list do
-    HTTPClient.get("/api/v2/users.json") |> _create_users
+    HTTPClient.get("/api/v2/users.json", users: [User])
   end
 
 
@@ -33,7 +33,7 @@ defmodule ZenEx.Model.User do
   """
   @spec show(integer) :: %User{}
   def show(id) when is_integer(id) do
-    HTTPClient.get("/api/v2/users/#{id}.json") |> _create_user
+    HTTPClient.get("/api/v2/users/#{id}.json", user: User)
   end
 
 
@@ -48,7 +48,7 @@ defmodule ZenEx.Model.User do
   """
   @spec create(%User{}) :: %User{}
   def create(%User{} = user) do
-    HTTPClient.post("/api/v2/users.json", %{user: user}) |> _create_user
+    HTTPClient.post("/api/v2/users.json", %{user: user}, user: User)
   end
 
 
@@ -63,7 +63,7 @@ defmodule ZenEx.Model.User do
   """
   @spec update(%User{}) :: %User{}
   def update(%User{} = user) do
-    HTTPClient.put("/api/v2/users/#{user.id}.json", %{user: user}) |> _create_user
+    HTTPClient.put("/api/v2/users/#{user.id}.json", %{user: user}, user: User)
   end
 
 
@@ -78,7 +78,7 @@ defmodule ZenEx.Model.User do
   """
   @spec create_or_update(%User{}) :: %User{}
   def create_or_update(%User{} = user) do
-    HTTPClient.post("/api/v2/users/create_or_update.json", %{user: user}) |> _create_user
+    HTTPClient.post("/api/v2/users/create_or_update.json", %{user: user}, user: User)
   end
 
 
@@ -93,7 +93,7 @@ defmodule ZenEx.Model.User do
   """
   @spec destroy(integer) :: %User{}
   def destroy(id) when is_integer(id) do
-    HTTPClient.delete("/api/v2/users/#{id}.json") |> _create_user
+    HTTPClient.delete("/api/v2/users/#{id}.json", user: User)
   end
 
 
@@ -108,7 +108,7 @@ defmodule ZenEx.Model.User do
   """
   @spec create_many(list(%User{})) :: %JobStatus{}
   def create_many(users) when is_list(users) do
-    HTTPClient.post("/api/v2/users/create_many.json", %{users: users}) |> Model.JobStatus._create_job_status
+    HTTPClient.post("/api/v2/users/create_many.json", %{users: users}, job_status: JobStatus)
   end
 
 
@@ -123,7 +123,7 @@ defmodule ZenEx.Model.User do
   """
   @spec update_many(list(%User{})) :: %JobStatus{}
   def update_many(users) when is_list(users) do
-    HTTPClient.put("/api/v2/users/update_many.json", %{users: users}) |> Model.JobStatus._create_job_status
+    HTTPClient.put("/api/v2/users/update_many.json", %{users: users}, job_status: JobStatus)
   end
 
 
@@ -138,7 +138,7 @@ defmodule ZenEx.Model.User do
   """
   @spec create_or_update_many(list(%User{})) :: %JobStatus{}
   def create_or_update_many(users) when is_list(users) do
-    HTTPClient.post("/api/v2/users/create_or_update_many.json", %{users: users}) |> Model.JobStatus._create_job_status
+    HTTPClient.post("/api/v2/users/create_or_update_many.json", %{users: users}, job_status: JobStatus)
   end
 
 
@@ -153,20 +153,6 @@ defmodule ZenEx.Model.User do
   """
   @spec destroy_many(list(integer)) :: %JobStatus{}
   def destroy_many(ids) when is_list(ids) do
-    HTTPClient.delete("/api/v2/users/destroy_many.json?ids=#{Enum.join(ids, ",")}") |> Model.JobStatus._create_job_status
-  end
-
-
-  @doc false
-  @spec _create_users(%HTTPotion.Response{}) :: list(%User{})
-  def _create_users(%HTTPotion.Response{} = res) do
-    res.body |> Poison.decode!(keys: :atoms, as: %{users: [%User{}]}) |> Map.get(:users)
-  end
-
-
-  @doc false
-  @spec _create_user(%HTTPotion.Response{}) :: %User{}
-  def _create_user(%HTTPotion.Response{} = res) do
-    res.body |> Poison.decode!(keys: :atoms, as: %{user: %User{}}) |> Map.get(:user)
+    HTTPClient.delete("/api/v2/users/destroy_many.json?ids=#{Enum.join(ids, ",")}", job_status: JobStatus)
   end
 end
--- spec/zen_ex/core/models/user_spec.exs
+++ spec/zen_ex/core/models/user_spec.exs
@@ -1,12 +1,11 @@
 defmodule ZenEx.Model.UserSpec do
   use ESpec
 
-  alias ZenEx.HTTPClient
   alias ZenEx.Entity.{User,JobStatus}
   alias ZenEx.Model
 
   let :json_users do
-    ~s({"users":[{"id":223443,"name":"Johnny Agent"},{"id":8678530,"name":"James A. Rosen"}]})
+    ~s({"count":2,"users":[{"id":223443,"name":"Johnny Agent"},{"id":8678530,"name":"James A. Rosen"}]})
   end
   let :users do
     [struct(User, %{id: 223443, name: "Johnny Agent"}), struct(User, %{id: 8678530, name: "James A. Rosen"})]
@@ -27,62 +26,53 @@ defmodule ZenEx.Model.UserSpec do
   let :response_job_status, do: %HTTPotion.Response{body: json_job_status()}
 
   describe "list" do
-    before do: allow HTTPClient |> to(accept :get, fn(_) -> response_users() end)
-    it do: expect Model.User.list |> to(eq users())
+    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_users() end)
+    it do: expect Model.User.list |> to(be_struct ZenEx.Collection)
+    it do: expect Model.User.list.entities |> to(eq users())
   end
 
   describe "show" do
-    before do: allow HTTPClient |> to(accept :get, fn(_) -> response_user() end)
+    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_user() end)
     it do: expect Model.User.show(user().id) |> to(eq user())
   end
 
   describe "create" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_user() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_user() end)
     it do: expect Model.User.create(user()) |> to(be_struct User)
   end
 
   describe "update" do
-    before do: allow HTTPClient |> to(accept :put, fn(_, _) -> response_user() end)
+    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_user() end)
     it do: expect Model.User.update(user()) |> to(be_struct User)
   end
 
   describe "create_or_update" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_user() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_user() end)
     it do: expect Model.User.create_or_update(user()) |> to(be_struct User)
   end
 
   describe "destroy" do
-    before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_user() end)
+    before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_user() end)
     it do: expect Model.User.destroy(user().id) |> to(be_struct User)
   end
 
   describe "create_many" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_job_status() end)
     it do: expect Model.User.create_many(users()) |> to(be_struct JobStatus)
   end
 
   describe "update_many" do
-    before do: allow HTTPClient |> to(accept :put, fn(_, _) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_job_status() end)
     it do: expect Model.User.update_many(users()) |> to(be_struct JobStatus)
   end
 
   describe "create_or_update_many" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_job_status() end)
     it do: expect Model.User.create_or_update_many(users()) |> to(be_struct JobStatus)
   end
 
   describe "destroy_many" do
-    before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_job_status() end)
     it do: expect Model.User.destroy_many(Enum.map(users(), &(&1.id))) |> to(be_struct JobStatus)
-  end
-
-  describe "_create_users" do
-    subject do: Model.User._create_users response_users()
-    it do: is_expected() |> to(eq users())
-  end
-
-  describe "_create_user" do
-    subject do: Model.User._create_user response_user()
-    it do: is_expected() |> to(eq user())
   end
 end
