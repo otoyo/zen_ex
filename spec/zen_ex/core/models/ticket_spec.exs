b781c4bf532a8659247e6b89415710d7bd5607a0
otoyo <hiroki.t.1988@gmail.com>
2017-06-17 15:02:47 +0900

    Use new HTTPClient at Model.Ticket

--- spec/zen_ex/core/models/ticket_spec.exs
+++ spec/zen_ex/core/models/ticket_spec.exs
@@ -1,12 +1,11 @@
 defmodule ZenEx.Model.TicketSpec do
   use ESpec
 
-  alias ZenEx.HTTPClient
   alias ZenEx.Entity.{Ticket,JobStatus}
   alias ZenEx.Model
 
   let :json_tickets do
-    ~s({"tickets":[{"id":35436,"subject":"Help I need somebody!"},{"id":20057623,"subject":"Not just anybody!"}]})
+    ~s({"count":2,"tickets":[{"id":35436,"subject":"Help I need somebody!"},{"id":20057623,"subject":"Not just anybody!"}]})
   end
   let :tickets do
     [struct(Ticket, %{id: 35436, subject: "Help I need somebody!"}),
@@ -30,48 +29,49 @@ defmodule ZenEx.Model.TicketSpec do
   let :response_404, do: %HTTPotion.Response{status_code: 404}
 
   describe "list" do
-    before do: allow HTTPClient |> to(accept :get, fn(_) -> response_tickets() end)
-    it do: expect Model.Ticket.list |> to(eq tickets())
+    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_tickets() end)
+    it do: expect Model.Ticket.list |> to(be_struct ZenEx.Collection)
+    it do: expect Model.Ticket.list.entities |> to(eq tickets())
   end
 
   describe "show" do
-    before do: allow HTTPClient |> to(accept :get, fn(_) -> response_ticket() end)
+    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_ticket() end)
     it do: expect Model.Ticket.show(ticket().id) |> to(eq ticket())
   end
 
   describe "create" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_ticket() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_ticket() end)
     it do: expect Model.Ticket.create(ticket()) |> to(be_struct Ticket)
   end
 
   describe "update" do
-    before do: allow HTTPClient |> to(accept :put, fn(_, _) -> response_ticket() end)
+    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_ticket() end)
     it do: expect Model.Ticket.update(ticket()) |> to(be_struct Ticket)
   end
 
   describe "destroy" do
     context "response status_code: 204" do
-      before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_204() end)
+      before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_204() end)
       it do: expect Model.Ticket.destroy(ticket().id) |> to(eq :ok)
     end
     context "response status_code: 404" do
-      before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_404() end)
+      before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_404() end)
       it do: expect Model.Ticket.destroy(ticket().id) |> to(eq :error)
     end
   end
 
   describe "create_many" do
-    before do: allow HTTPClient |> to(accept :post, fn(_, _) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_job_status() end)
     it do: expect Model.Ticket.create_many(tickets()) |> to(be_struct JobStatus)
   end
 
   describe "update_many" do
-    before do: allow HTTPClient |> to(accept :put, fn(_, _) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_job_status() end)
     it do: expect Model.Ticket.update_many(tickets()) |> to(be_struct JobStatus)
   end
 
   describe "destroy_many" do
-    before do: allow HTTPClient |> to(accept :delete, fn(_) -> response_job_status() end)
+    before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_job_status() end)
     it do: expect Model.Ticket.destroy_many(Enum.map(tickets(), &(&1.id))) |> to(be_struct JobStatus)
   end
 
@@ -84,15 +84,5 @@ defmodule ZenEx.Model.TicketSpec do
       subject do: Model.Ticket._desc_to_comment ticket()
       it do: is_expected() |> to(eq Map.merge(ticket(), %{comment: %{body: ticket().description}}))
     end
-  end
-
-  describe "_create_tickets" do
-    subject do: Model.Ticket._create_tickets response_tickets()
-    it do: is_expected() |> to(eq tickets())
-  end
-
-  describe "_create_ticket" do
-    subject do: Model.Ticket._create_ticket response_ticket()
-    it do: is_expected() |> to(eq ticket())
   end
 end
