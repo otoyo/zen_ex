34a3f838705dc6d49dfa4d486c3c0c652c6da3c5
otoyo <hiroki.t.1988@gmail.com>
2017-06-17 14:29:32 +0900

    Build and return entity in HTTPClient

--- lib/zen_ex/http_client.ex
+++ lib/zen_ex/http_client.ex
@@ -4,24 +4,30 @@ defmodule ZenEx.HTTPClient do
 
   @content_type "application/json"
 
-  def get(endpoint) do
-    build_url(endpoint)
-    |> HTTPotion.get([basic_auth: basic_auth()])
-  end
+  alias ZenEx.Collection
 
-  def post(endpoint, %{} = param) do
+  def get("https://" <> _ = url, decode_as) do
+    url
+    |> HTTPotion.get([basic_auth: basic_auth()])
+    |> _build_entity(decode_as)
+  end
+  def get(endpoint, decode_as), do: endpoint |> build_url |> get(decode_as)
+
+  def post(endpoint, %{} = param, decode_as) do
     build_url(endpoint)
     |> HTTPotion.post([body: Poison.encode!(param), headers: ["Content-Type": @content_type], basic_auth: basic_auth()])
+    |> _build_entity(decode_as)
   end
 
-  def put(endpoint, %{} = param) do
+  def put(endpoint, %{} = param, decode_as) do
     build_url(endpoint)
     |> HTTPotion.put([body: Poison.encode!(param), headers: ["Content-Type": @content_type], basic_auth: basic_auth()])
+    |> _build_entity(decode_as)
   end
 
+  def delete(endpoint, decode_as), do: delete(endpoint) |> _build_entity(decode_as)
   def delete(endpoint) do
-    build_url(endpoint)
-    |> HTTPotion.delete([basic_auth: basic_auth()])
+    build_url(endpoint) |> HTTPotion.delete([basic_auth: basic_auth()])
   end
 
   def build_url(endpoint) do
@@ -30,5 +36,17 @@ defmodule ZenEx.HTTPClient do
 
   def basic_auth do
     {"#{Application.get_env(:zen_ex, :user)}/token", "#{Application.get_env(:zen_ex, :api_token)}"}
+  end
+
+  def _build_entity(%HTTPotion.Response{} = res, [{key, [module]}]) do
+    {entities, page} =
+      res.body
+      |> Poison.decode!(keys: :atoms, as: %{key => [struct(module)]})
+      |> Map.pop(key)
+
+    struct(Collection, Map.merge(page, %{entities: entities, decode_as: [{key, [module]}]}))
+  end
+  def _build_entity(%HTTPotion.Response{} = res, [{key, module}]) do
+    res.body |> Poison.decode!(keys: :atoms, as: %{key => struct(module)}) |> Map.get(key)
   end
 end
