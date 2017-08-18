8ee7a8b3131fe53d639018154f5e41480d8c9c93
otoyo <hiroki.t.1988@gmail.com>
2017-08-18 21:26:39 +0900

    Ver up mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ZenEx.Mixfile do
 
   def project do
     [app: :zen_ex,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.4",
      description: @description,
      package: package(),
