16a394c2eb720a9333cfb6199826aa7fb81b4761
otoyo <hiroki.t.1988@gmail.com>
2020-05-16 12:18:48 +0900

    Add SearchQuery

---
+++ lib/zen_ex/search_query.ex
@@ -0,0 +1,34 @@
+defmodule ZenEx.SearchQuery do
+
+  @moduledoc """
+  Query for Search API
+  """
+
+  @doc """
+  Build query from opts.
+
+  ## Examples
+
+      iex> ZenEx.SearchQuery.build(email: "first.last@email.com", foo: "bar")
+      "email:first.last@email.com foo:bar"
+
+  """
+  @spec build(map()) :: String.t
+  def build(opts) when is_map(opts) do
+    fragments =
+      opts
+      |> Enum.map(fn {k,v} ->
+        case v do
+          nil                         -> nil
+          values when is_list(values) -> "#{Atom.to_string(k)}:#{Enum.join(values, ",")}"
+          _                           -> "#{Atom.to_string(k)}:#{v}"
+        end
+      end)
+      |> Enum.reject(&is_nil/1)
+
+    case fragments do
+      []  -> ""
+      _   -> Enum.join(fragments, " ")
+    end
+  end
+end
---
+++ spec/zen_ex/search_query_spec.exs
@@ -0,0 +1,25 @@
+defmodule ZenEx.SearchQuerySpec do
+  use ESpec
+
+  alias ZenEx.SearchQuery
+
+  let :email, do: "first.last@example.com"
+  let :role, do: "agent"
+
+  describe "build" do
+    describe "with params" do
+      subject do: SearchQuery.build(%{email: email(), role: role()})
+      it do: is_expected() |> to(eq "email:first.last@example.com role:agent")
+    end
+
+    describe "without params" do
+      subject do: SearchQuery.build(%{})
+      it do: is_expected() |> to(eq "")
+    end
+
+    describe "with nil params" do
+      subject do: SearchQuery.build(%{foo: nil})
+      it do: is_expected() |> to(eq "")
+    end
+  end
+end
