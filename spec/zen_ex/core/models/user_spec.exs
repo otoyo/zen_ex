d073300f257c59a196fe5e95701f58eee5eed6d3
otoyo <hiroki.t.1988@gmail.com>
2021-04-10 16:02:09 +0900

    Format

--- spec/zen_ex/core/models/user_spec.exs
+++ spec/zen_ex/core/models/user_spec.exs
@@ -1,22 +1,27 @@
 defmodule ZenEx.Model.UserSpec do
   use ESpec
 
-  alias ZenEx.Entity.{User,JobStatus}
+  alias ZenEx.Entity.{User, JobStatus}
   alias ZenEx.Model
 
   let :json_users do
     ~s({"count":2,"users":[{"id":223443,"name":"Johnny Agent"},{"id":8678530,"name":"James A. Rosen"}]})
   end
+
   let :users do
-    [struct(User, %{id: 223443, name: "Johnny Agent"}), struct(User, %{id: 8678530, name: "James A. Rosen"})]
+    [
+      struct(User, %{id: 223_443, name: "Johnny Agent"}),
+      struct(User, %{id: 8_678_530, name: "James A. Rosen"})
+    ]
   end
 
-  let :json_user, do: ~s({"user":{"id":223443,"name":"Johnny Agent"}})
-  let :user, do: struct(User, %{id: 223443, name: "Johnny Agent"})
+  let(:json_user, do: ~s({"user":{"id":223443,"name":"Johnny Agent"}}))
+  let(:user, do: struct(User, %{id: 223_443, name: "Johnny Agent"}))
 
   let :json_job_status do
     ~s({"job_status":{"id":"8b726e606741012ffc2d782bcb7848fe","status":"completed"}})
   end
+
   let :job_status do
     struct(JobStatus, %{id: "8b726e606741012ffc2d782bcb7848fe", status: "completed"})
   end
@@ -25,73 +30,89 @@ defmodule ZenEx.Model.UserSpec do
     ~s({"count":2,"results":[{"id":223443,"name":"Johnny Agent"},{"id":8678530,"name":"James A. Rosen"}]})
   end
 
-  let :response_user, do: %HTTPotion.Response{body: json_user()}
-  let :response_users, do: %HTTPotion.Response{body: json_users()}
-  let :response_search_users, do: %HTTPotion.Response{body: json_search_users()}
-  let :response_job_status, do: %HTTPotion.Response{body: json_job_status()}
+  let(:response_user, do: %HTTPotion.Response{body: json_user()})
+  let(:response_users, do: %HTTPotion.Response{body: json_users()})
+  let(:response_search_users, do: %HTTPotion.Response{body: json_search_users()})
+  let(:response_job_status, do: %HTTPotion.Response{body: json_job_status()})
 
   describe "list" do
-    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_users() end)
-    it do: expect Model.User.list |> to(be_struct ZenEx.Collection)
-    it do: expect Model.User.list.entities |> to(eq users())
+    before(do: allow(HTTPotion |> to(accept(:get, fn _, _ -> response_users() end))))
+    it(do: expect(Model.User.list() |> to(be_struct(ZenEx.Collection))))
+    it(do: expect(Model.User.list().entities |> to(eq(users()))))
   end
 
   describe "show" do
-    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_user() end)
-    it do: expect Model.User.show(user().id) |> to(eq user())
+    before(do: allow(HTTPotion |> to(accept(:get, fn _, _ -> response_user() end))))
+    it(do: expect(Model.User.show(user().id) |> to(eq(user()))))
   end
 
   describe "create" do
-    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_user() end)
-    it do: expect Model.User.create(user()) |> to(be_struct User)
+    before(do: allow(HTTPotion |> to(accept(:post, fn _, _ -> response_user() end))))
+    it(do: expect(Model.User.create(user()) |> to(be_struct(User))))
   end
 
   describe "update" do
-    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_user() end)
-    it do: expect Model.User.update(user()) |> to(be_struct User)
+    before(do: allow(HTTPotion |> to(accept(:put, fn _, _ -> response_user() end))))
+    it(do: expect(Model.User.update(user()) |> to(be_struct(User))))
   end
 
   describe "create_or_update" do
-    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_user() end)
-    it do: expect Model.User.create_or_update(user()) |> to(be_struct User)
+    before(do: allow(HTTPotion |> to(accept(:post, fn _, _ -> response_user() end))))
+    it(do: expect(Model.User.create_or_update(user()) |> to(be_struct(User))))
   end
 
   describe "destroy" do
-    before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_user() end)
-    it do: expect Model.User.destroy(user().id) |> to(be_struct User)
+    before(do: allow(HTTPotion |> to(accept(:delete, fn _, _ -> response_user() end))))
+    it(do: expect(Model.User.destroy(user().id) |> to(be_struct(User))))
   end
 
   describe "create_many" do
-    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_job_status() end)
-    it do: expect Model.User.create_many(users()) |> to(be_struct JobStatus)
+    before(do: allow(HTTPotion |> to(accept(:post, fn _, _ -> response_job_status() end))))
+    it(do: expect(Model.User.create_many(users()) |> to(be_struct(JobStatus))))
   end
 
   describe "update_many" do
-    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response_job_status() end)
-    it do: expect Model.User.update_many(users()) |> to(be_struct JobStatus)
+    before(do: allow(HTTPotion |> to(accept(:put, fn _, _ -> response_job_status() end))))
+    it(do: expect(Model.User.update_many(users()) |> to(be_struct(JobStatus))))
   end
 
   describe "create_or_update_many" do
-    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response_job_status() end)
-    it do: expect Model.User.create_or_update_many(users()) |> to(be_struct JobStatus)
+    before(do: allow(HTTPotion |> to(accept(:post, fn _, _ -> response_job_status() end))))
+    it(do: expect(Model.User.create_or_update_many(users()) |> to(be_struct(JobStatus))))
   end
 
   describe "destroy_many" do
-    before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response_job_status() end)
-    it do: expect Model.User.destroy_many(Enum.map(users(), &(&1.id))) |> to(be_struct JobStatus)
+    before(do: allow(HTTPotion |> to(accept(:delete, fn _, _ -> response_job_status() end))))
+
+    it(
+      do: expect(Model.User.destroy_many(Enum.map(users(), & &1.id)) |> to(be_struct(JobStatus)))
+    )
   end
 
   describe "search" do
-    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response_search_users() end)
+    before(do: allow(HTTPotion |> to(accept(:get, fn _, _ -> response_search_users() end))))
 
     context "when argument is a map" do
-      it do: expect Model.User.search(%{email: "first.last@example.com"}) |> to(be_struct ZenEx.Collection)
-      it do: expect Model.User.search(%{email: "first.last@example.com"}).entities |> to(eq users())
+      it(
+        do:
+          expect(
+            Model.User.search(%{email: "first.last@example.com"})
+            |> to(be_struct(ZenEx.Collection))
+          )
+      )
+
+      it(
+        do:
+          expect(
+            Model.User.search(%{email: "first.last@example.com"}).entities
+            |> to(eq(users()))
+          )
+      )
     end
 
     context "when argument is a string" do
-      it do: expect Model.User.search("my_string") |> to(be_struct ZenEx.Collection)
-      it do: expect Model.User.search("my_string").entities |> to(eq users())
+      it(do: expect(Model.User.search("my_string") |> to(be_struct(ZenEx.Collection))))
+      it(do: expect(Model.User.search("my_string").entities |> to(eq(users()))))
     end
   end
 end
