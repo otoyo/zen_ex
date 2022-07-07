1ff751c3c1ec86112b3182471e93a9b1072ae109
HirokiToyokawa <hiroki.toyokawa@mixi.co.jp>
2022-07-08 08:56:36 +0900

    Bump v0.6.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ZenEx.Mixfile do
   def project do
     [
       app: :zen_ex,
-      version: "0.6.0",
+      version: "0.6.1",
       elixir: "~> 1.7",
       description: @description,
       package: package(),
